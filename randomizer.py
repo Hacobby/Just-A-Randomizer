0e15ce5858cd2adeb0ebdb9bb8d002318a6b73e9
NotAKing <42632208+Hacobby@users.noreply.github.com>
2025-09-03 21:30:18 -0500

    Initial upload of repository

---
+++ randomizer.py
@@ -0,0 +1,12 @@
+gate = True
+while gate:
+    print("--- Ultimate Randomizer ---")
+    entries = input("Ingrese la cantidad de entradas: ")
+    if entries == "exit":
+        gate = False
+    entries = int(entries)
+    actors = {}
+    for i in range(0, entries):
+        print(i)
+        actors
+    rolls = int(input("Ingrese la cantidad de rolls: "))
