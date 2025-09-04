77f311e9d098403c3c899be7f4a996f8504e0e90
Hacobby <elkinhacobby@gmail.com>
2025-09-03 22:04:04 -0500

    main.py update
    Finished the core of the randomizer, future features will depend on this

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+import random
 gate = True
+
 while gate:
     print("--- Ultimate Randomizer ---")
     entries = input("Ingrese la cantidad de entradas: ")
     if entries == "exit":
         gate = False
+        print("Goodbye")
+        break
     entries = int(entries)
-    actors = {}
+
+    actors = [""] * entries
     for i in range(0, entries):
-        print(i)
-        actors
+        #print(i)
+        actors[i] = input("Ingrese el nombre del actor: ")
+    stage = {name : 0 for name in actors}
+
     rolls = int(input("Ingrese la cantidad de rolls: "))
+    for i in range(rolls):
+        rWinner = random.choice(actors)
+        stage[rWinner] += 1
+
+    print("Resultados: ")
+    for i in stage:
+        print(i, stage[i])
