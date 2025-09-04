39f5389da7502e32afeace049055a523b2d4f61e
Hacobby <elkinhacobby@gmail.com>
2025-09-04 15:06:17 -0500

    miscellaneous update
    Actualice los textos que se muestral al usuario y como pide los datos de los actores, también una forma de pausar el avance a la siguiente iteración y una forma de salir antes de  esta

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import random
+import os
+import platform
+import time
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 gate = True
 
 while gate:
-    print("--- Ultimate Randomizer ---")
-    entries = input("Ingrese la cantidad de entradas: ")
+    print("--- Just A Randomizer ---")
+    entries = input("Cantidad de entradas: ")
     if entries == "exit":
         gate = False
         print("Goodbye")
@@ -12,15 +19,25 @@ while gate:
 
     actors = [""] * entries
     for i in range(0, entries):
-        #print(i)
-        actors[i] = input("Ingrese el nombre del actor: ")
+        actors[i] = input("Nombre del actor [" + str(i+1) +"]: ")
     stage = {name : 0 for name in actors}
 
-    rolls = int(input("Ingrese la cantidad de rolls: "))
+    rolls = int(input("Cantidad de rolls: "))
     for i in range(rolls):
         rWinner = random.choice(actors)
         stage[rWinner] += 1
 
     print("Resultados: ")
     for i in stage:
+        time.sleep(1)
         print(i, stage[i])
+
+    time.sleep(2)
+
+    entries = input("Enter para continuar")
+    if entries == "exit":
+        gate = False
+        print("Goodbye")
+        break
+
+    clear()
